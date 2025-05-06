d94afacf0cfffbecb7c34a5bc17f7a7229040379
Tesao <shimeji9@gmail.com>
2025-05-06 21:11:37 +0900

    header menuを追加

--- scripts/components/header-component.js
+++ scripts/components/header-component.js
@@ -1,5 +1,5 @@
 class HeaderComponent extends HTMLElement {
-  constructor() {
+    constructor() {
     super();
     this.shadow = this.attachShadow({mode: 'closed'});
     this.template = document.createElement('template');
@@ -19,11 +19,40 @@ class HeaderComponent extends HTMLElement {
           font-weight: 700;
           font-size: 1rem;
         }
+        .header-menu {
+          margin-left: auto;
+          display: flex;
+          gap: 1rem;
+          font-size: 0.8rem;
+          color: #333;
+          a {
+            text-decoration: none;
+          }
+        }
+        .header-humberger {
+          display: none;
+        }
+      }
+
+      @media (max-width: 900px) {
+        :host .header-title {
+          font-size: 0.8rem;
+        }
+        :host .header-menu {
+          display: none;
+        }
+        :host .header-humberger {
+          display: block;
+          cursor: pointer;
+        }
       }
       </style>
       <div class='header-wrapper'>
         <img class='header-brand' src='/assets/logo.svg' height='30px' />
         <span class='header-title'>三宮.dev イベント提案サイト</span>
+        <div class='header-menu'>
+          <a href='https://sannomiya.dev' target="_blank">三宮.devについて</a>
+        </div>
       </div>
     `;
   }
