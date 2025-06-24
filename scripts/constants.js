f0a45c690cae3c13c69cb887fe021daa72f6f7da
tesao <shimeji9@gmail.com>
2025-06-24 20:42:39 +0900

    updated gas api

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,1 +1,1 @@
-export const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbx4YADPhrIuNIhEvwnsFI26vP7caO66ehlBV-NPsloUsVETiGBNLYAkLa0sYkBbLJBm/exec';
+export const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwhRUS7XX47mTtNeb5zQ_LElH30wv4OUZNvhdQBAVNYxHq2y2dsZYzRIcFCjKIxdG8M/exec';
